07a8afdf8c6404f0f9fab6b3b6ad4f2bc5887618
Jen Tam <jennifer.k.tam@gmail.com>
2017-06-23 13:43:03 -0700

    feat(schemas): started defining models and associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,3 +1,13 @@
-require('./user');
-require('./err');
-require('./result');
+const User = require('./user');
+const Err = require('./err');
+const Link = require('./link');
+const Vote = require('./vote');
+
+User.belongsToMany(Err, {through: 'user_err'});
+User.belongsToMany(Link, {through: 'user_link'});
+User.belongsToMany(Vote, {through: 'user_vote'});
+
+Err.belongsToMany(Link, {through: 'err_link'});
+
+Vote.belongsToMany(Link, {through: 'vote_link'});
+
--- server/db/models/link.js
+++ server/db/models/link.js
@@ -1,13 +1,16 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-module.exports = db.define('err', {
-  type: {
-    type: Sequelize.ENUM('TypeError', 'SyntaxError', 'ReferenceError', 'EvalError', 'InternalError', 'RangeError', 'URIError'),
-  },
-  message: {
+module.exports = db.define('link', {
+  link: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  dateModified: {
+    type: Sequelize.DATE,
+  },
+  dateCreated: {
+    type: Sequelize.DATE,
   },
 }, {
   instanceMethods: {
