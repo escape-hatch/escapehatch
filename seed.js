5153da62968ef33492f445af678613d6386e2090
CindySchalit <cindyschalit@gmail.com>
2017-06-23 18:41:52 -0700

    Feat(Err Model): seed errors

--- seed.js
+++ seed.js
@@ -26,7 +26,25 @@ const data = {
     },
   ],
   err: [
-
+  {
+    type: 'TypeError',
+    message: `Cannot read property 'be' of undefined`,
+    stack: 'at Context.<anonymous> (test/test.js:18:16)',
+  },
+  {
+    type: 'SyntaxError',
+    message: `Unexpected token ,`,
+    stack: `at createScript (vm.js:53:10)
+    at Object.runInThisContext (vm.js:95:10)
+    at Module._compile (module.js:543:28)
+    at Object.Module._extensions..js (module.js:580:10)
+    at Module.load (module.js:488:32)
+    at tryModuleLoad (module.js:447:12)
+    at Function.Module._load (module.js:439:3)
+    at Module.runMain (module.js:605:10)
+    at run (bootstrap_node.js:423:7)
+    at startup (bootstrap_node.js:147:9)`,
+  },
   ]
 }
 
@@ -36,6 +54,7 @@ db.sync()
   // return db.sync()
 })
 .then( () => {return User.bulkCreate(data.user)})
+.then( () => {return Err.bulkCreate(data.err)})
 .then(function () {
   console.log("Finished inserting data");
 })
