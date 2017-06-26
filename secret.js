2bc814428c1625c101e23c626810413ab4d9b6f7
Jen Tam <jennifer.k.tam@gmail.com>
2017-06-26 09:33:34 -0700

    feat(.gitignore): updated

--- secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-GOOGLE_CLIENT_ID: '66185949939-sdnaq0p78j38c2pi8p9ql5f5fg2gn0e3.apps.googleusercontent.com',
- GOOGLE_CLIENT_SECRET: 'E4AasA9TbyTdvYbNxLBXn_n4',
- GOOGLE_CALLBACK: '/auth/google/callback'
-}
