390397c382d78811a3f22556f909e301cca6770a
Josh White <whitejosh42@gmail.com>
2017-06-27 11:42:47 -0500

    feat(API): serve all data from both APIs

--- server/api/search/index.js
+++ server/api/search/index.js
@@ -5,19 +5,19 @@ const stackAppFormatter = require('./stackapp/formatter')
 const base64url = require('base64-url')
 const stackApp = require('./stackapp/request')
 const Promise = require('bluebird')
+
 // what happens if any single 3rd party API call fails?
 // service param?
 
 router.get('/:err', (req, res, next) => {
   const userErr = base64url.decode(req.params.err)
-  console.log(userErr)
-  res.send(userErr)
 
   Promise.all([github(userErr), stackApp(userErr)])
   .spread((githubResults, stackAppResults) => {
     const stackData = stackAppFormatter(stackAppResults.data.items)
-    // githubFormatter(githubResults.items)
-    res.json(stackData)
+    const gitData = githubFormatter(githubResults.items)
+    const data = { stackData, gitData }
+    res.json(data)
   })
   // .then(response => res.json(response))
   .catch(err => console.error(err))
