aea221e3acb6c32814eb4776fab14919042cc459
Jen Tam <jennifer.k.tam@gmail.com>
2017-06-28 08:56:10 -0700

    feat(Google Authentication Workaround): Temporarily set up try catch method for secret file in order to let Travis build successfully

--- server/auth/google.js
+++ server/auth/google.js
@@ -2,29 +2,38 @@ const passport = require('passport');
 const router = require('express').Router();
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('../db/models/user');
-const secret = require('../../secret');
+let secret;
 
-const googleConfig = {
-  clientID: secret.GOOGLE_CLIENT_ID,
-  clientSecret: secret.GOOGLE_CLIENT_SECRET,
-  callbackURL: secret.GOOGLE_CALLBACK
-};
+try {
+  secret = require('../../secret');
+}
+catch(err){
+  console.log('No secret file found. Skipping OAuth.')
+}
 
-const strategy = new GoogleStrategy(googleConfig, (token, refreshToken, profile, done) => {
-  const googleId = profile.id;
-  const name = profile.displayName;
-  const email = profile.emails[0].value;
+if(secret) {
+  const googleConfig = {
+    clientID: secret.GOOGLE_CLIENT_ID,
+    clientSecret: secret.GOOGLE_CLIENT_SECRET,
+    callbackURL: secret.GOOGLE_CALLBACK
+  };
 
-  User.find({ where: { googleId } })
-    .then(user => user ?
-      done(null, user) :
-      User.create({ name, email, googleId })
-        .then(user => done(null, user))
-    )
-    .catch(done);
-});
+  const strategy = new GoogleStrategy(googleConfig, (token, refreshToken, profile, done) => {
+    const googleId = profile.id;
+    const name = profile.displayName;
+    const email = profile.emails[0].value;
 
-passport.use(strategy);
+    User.find({ where: { googleId } })
+      .then(user => user ?
+        done(null, user) :
+        User.create({ name, email, googleId })
+          .then(user => done(null, user))
+      )
+      .catch(done);
+  });
+
+  passport.use(strategy);
+}
 
 module.exports = router
   .get('/', passport.authenticate('google', { scope: 'email' }))
