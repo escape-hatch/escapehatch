4cfbcbefa4f87ca37be30c0d54cac63b53ab9c3d
CindySchalit <cindyschalit@gmail.com>
2017-06-29 13:01:11 -0700

    Feat(LinksTest): add test for git results
    add test to verify that Links Component renders git results

--- client/components/Links.spec.js
+++ client/components/Links.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import {
   renderIntoDocument,
   findRenderedDOMComponentWithClass,
+  scryRenderedDOMComponentsWithClass,
   scryRenderedComponentsWithType
 } from 'react-addons-test-utils';
 import { expect } from 'chai';
@@ -18,12 +20,10 @@ describe('Links Component', () => {
     git = ['i\'m just a link', 'yes i\'m only a link'];
     stack = ['and i\'m sitting here on capitol hill'];
 
-    component = renderIntoDocument(
-      <Links
-        git={git}
-        stack={stack}
-      />
-    );
+    component = ReactTestUtils.renderIntoDocument(<Links
+      git={git}
+      stack={stack}
+    />);
   });
 
   it('should render a div with "links" class', () => {
@@ -32,25 +32,9 @@ describe('Links Component', () => {
     expect(linksElem).to.be.ok;
   });
 
-  xit('should render a list item for each todo item', () => {
-    const git = ['i\'m just a link', 'yes i\'m only a link'];
-    const stack = ['and i\'m sitting here on capitol hill'];
+  it('should render a list item for each git result', () => {
+    const linksElem = scryRenderedDOMComponentsWithClass(component, 'gitResults');
 
-    const component = renderIntoDocument(
-      <Links
-        git={git}
-        stack={stack}
-      />
-    );
-
-    const todosEle = scryRenderedComponentsWithType(component, Todo);
-    const todo1 = ReactDOM.findDOMNode(todosEle[0]).textContent;
-    const todo2 = ReactDOM.findDOMNode(todosEle[1]).textContent;
-    const todo3 = ReactDOM.findDOMNode(todosEle[2]).textContent;
-
-    expect(todosEle.length).to.equal(3);
-    expect(todo1).to.equal('Mow lawn');
-    expect(todo2).to.equal('Walk dog');
-    expect(todo3).to.equal('Read book');
+    expect(linksElem.length).to.equal(2);
   });
 });
