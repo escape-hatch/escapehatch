0ef0f5487a48190817a9311756f98ce2b54d7479
Jen Tam <jennifer.k.tam@gmail.com>
2017-06-30 14:20:56 -0700

    chore(Backend Links): Updated userId to req.session.passport.user to allow voting only for users logged in

--- server/api/links.js
+++ server/api/links.js
@@ -26,8 +26,8 @@ router
 
 // when user clicks "solved/helpful/etc":
 .put('/:vendor', (req, res, next) => {
-  const userId = 1; //need to change back to req.session.id
-
+  const userId = req.session.passport.user;
+  console.log("userId", userId)
   const info = {
     error: req.body.error,
     vendor: req.body.vendor,
