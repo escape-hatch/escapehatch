f5be13e823ad64a89abe52b1c04f9baa534e7c65
CindySchalit <cindyschalit@gmail.com>
2017-07-03 12:47:41 -0700

    Test(Links): fix indentation and update wording

--- client/components/Links2.spec.js
+++ client/components/Links2.spec.js
@@ -41,49 +41,53 @@ describe('Links Component - List Items', function () {
     wrapper = shallow(<Links git={git} stack={stack} dispatchUpvote={spy} />);
   });
 
-  it('renders the title of each git and stack search result', () => {
-    expect(wrapper.text()).to.contain(`i\'m just a link`);
-    expect(wrapper.text()).to.contain(`and i\'m sittin\' here on capitol hill`);
-  });
+  describe('Contents', () => {
+    it('renders the title of each git and stack search result', () => {
+      expect(wrapper.text()).to.contain(`i\'m just a link`);
+      expect(wrapper.text()).to.contain(`and i\'m sittin\' here on capitol hill`);
+    });
 
-  it('renders the last activity date of each git and stack search result', () => {
-    expect(wrapper.text()).to.contain('2017-06-22T18:54:57Z');
-    expect(wrapper.text()).to.contain('Sun Feb 22 2015');
-  });
+    it('renders the last activity date of each git and stack search result', () => {
+      expect(wrapper.text()).to.contain('2017-06-22T18:54:57Z');
+      expect(wrapper.text()).to.contain('Sun Feb 22 2015');
+    });
 
-  it('renders the status of each git search result', () => {
-    expect(wrapper.text()).to.contain('closed');
-  });
+    it('renders the status of each git search result', () => {
+      expect(wrapper.text()).to.contain('closed');
+    });
 
-  it('renders the number of comments for each git search result', () => {
-    expect(wrapper.text()).to.contain('2');
-  });
+    it('renders the number of comments for each git search result', () => {
+      expect(wrapper.text()).to.contain('2');
+    });
 
-  it('renders the number of views for each stack search result', () => {
-    expect(wrapper.text()).to.contain('47');
-  });
-
-  it('calls dispatchUpvote handler when clicked on git result', () => {
-    wrapper = shallow(<Links git={git} dispatchUpvote={spy} />);
-    wrapper.find('.upvote').simulate('click');
-    expect(spy).to.have.property('callCount', 1);
+    it('renders the number of views for each stack search result', () => {
+      expect(wrapper.text()).to.contain('47');
+    });
   })
 
-  it('calls dispatchDownvote handler when clicked on git result', () => {
-    wrapper = shallow(<Links git={git} dispatchDownvote={spy} />);
-    wrapper.find('.downvote').simulate('click');
-    expect(spy).to.have.property('callCount', 1);
-  })
+  describe('Voting Buttons', () => {
+    it('calls dispatchUpvote handler when user clicks to upvote git result', () => {
+      wrapper = shallow(<Links git={git} dispatchUpvote={spy} />);
+      wrapper.find('.upvote').simulate('click');
+      expect(spy).to.have.property('callCount', 1);
+    })
 
-  it('calls dispatchUpvote handler when clicked on Stack Overflow result', () => {
-    wrapper = shallow(<Links stack={stack} dispatchUpvote={spy} />);
-    wrapper.find('.upvote').simulate('click');
-    expect(spy).to.have.property('callCount', 1);
-  })
+    it('calls dispatchDownvote handler when user clicks to downvote git result', () => {
+      wrapper = shallow(<Links git={git} dispatchDownvote={spy} />);
+      wrapper.find('.downvote').simulate('click');
+      expect(spy).to.have.property('callCount', 1);
+    })
 
-  it('calls dispatchDownvote handler when clicked on Stack Overflow result', () => {
-    wrapper = shallow(<Links stack={stack} dispatchDownvote={spy} />);
-    wrapper.find('.downvote').simulate('click');
-    expect(spy).to.have.property('callCount', 1);
+    it('calls dispatchUpvote handler when user clicks to upvote stack overflow result', () => {
+      wrapper = shallow(<Links stack={stack} dispatchUpvote={spy} />);
+      wrapper.find('.upvote').simulate('click');
+      expect(spy).to.have.property('callCount', 1);
+    })
+
+    it('calls dispatchDownvote handler when user clicks to downvote stack overflow result', () => {
+      wrapper = shallow(<Links stack={stack} dispatchDownvote={spy} />);
+      wrapper.find('.downvote').simulate('click');
+      expect(spy).to.have.property('callCount', 1);
+    })
   })
 });
