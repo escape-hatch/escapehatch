f0e53d560a6899449e2c79e5958ffc9ea19900d5
CindySchalit <cindyschalit@gmail.com>
2017-07-06 10:13:04 -0700

    Feat(UserPage): send only links that user has upvoted to front end

--- server/api/upvotes.js
+++ server/api/upvotes.js
@@ -9,11 +9,20 @@ router
   const userId = req.session.passport.user
 
   user_links.findAll()
-  .then(votes => {
-    const userUpvotes = votes.filter(v => v.userId === userId && v.vote === 'upvote')
-    res.send(userUpvotes)
-  })
-  .catch(next);
+    .then(votes => {
+      const userUpvotes = votes.filter(v => v.userId === userId && v.vote === 'upvote')
+      return userUpvotes
+    })
+    .then(userUpvotes => {
+      const linkIds = userUpvotes.map(u => u.linkId)
+      const fetchLinks = linkIds.map(linkId => {
+        return Link.findById(linkId)
+      })
+
+      Promise.all(fetchLinks)
+        .then(links => res.send(links))
+    })
+    .catch(next);
 })
 
 module.exports = router;
