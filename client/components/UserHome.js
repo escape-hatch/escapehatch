a6804eb1c56b42e246054fe6f90446f9c24a0b9f
CindySchalit <cindyschalit@gmail.com>
2017-07-06 11:08:33 -0700

    Chore(UserPage): map upvoted links to list items

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const UserHome = props => {
-  const { firstName, email } = props;
+  const { firstName, email, upvotedLinks } = props;
 
   return (
     <div>
@@ -15,13 +15,21 @@ const UserHome = props => {
             <h3>Welcome!</h3>
       }
       <h4>Below are links you have previously found helpful:</h4>
+      <ul>
+      {
+        upvotedLinks && upvotedLinks.map(l => {
+          return <li key={ l.id }><a href={l.link}>{l.link} <span className="glyphicon glyphicon-log-in"></span></a></li>
+        })
+      }
+      </ul>
     </div>
   );
 };
 
-const mapState = ({ user }) => ({
+const mapState = ({ user, link }) => ({
   firstName: user.firstName,
   email: user.email,
+  upvotedLinks: link.upvotedLinks,
 });
 
 export default connect(mapState)(UserHome);
