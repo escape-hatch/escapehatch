6e486c90e865511911706fb0efb06c0c2d646203
Josh White <whitejosh42@gmail.com>
2017-07-06 09:08:24 -0500

    chore(links): refactored git and stack results into dumb components

--- client/components/Links.js
+++ client/components/Links.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import cssLinks from './scss/Links.scss';
 import { updateVote } from '../reducer/link';
-import { Link } from 'react-router';
+import GitComponent from './GitComponent';
+import StackComponent from './StackComponent';
 
 // Component //
 class Links extends React.Component {
@@ -21,151 +22,51 @@ class Links extends React.Component {
   render() {
     const stack = this.props.stack;
     const git = this.props.git;
-    const error = this.props.error
-    const user = this.props.user
+    const error = this.props.error;
+    const user = this.props.user;
 
     return (
       <div className="links">
         <h1>Search Results for "{ error }"</h1>
         <ul>
-        {
-          git && git.map(l => {
-            return (
-              <li key={ l.vendor_id } className="gitResults results">
-                <div className="center-block" style={{textAlign: 'left'}}>
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-12">
-                        <div className="panel panel-primary">
-                          <div className="panel-heading">
-                            <h2 className="panel-title" style={{display: 'inline'}}>
-                              <img src="/img/github-icon.png" className="vendorIcon" />
-                              <a href={l.url}>{l.title} <span className="glyphicon glyphicon-log-in"></span></a>
-                            </h2>
-                            <div className="" style={{float:'right', padddinRight: '30px'}}>Status: {l.status}</div>
-                          </div>
-
-                          <div className="panel-body">
-
-                            <div className="row">
-                              <div className="col-md-6">Last Modified: {l.modified}</div>
-
-                              <div className="col-md-6">
-                                 Score: {l.comments}
-                              </div>
-                            </div>
-
-                            <div className="row">
-                              <div className="body-heading"></div>
-                            </div>
-
-                            <div className="row">
-                              <div className="col-md-12 body">
-                                <div dangerouslySetInnerHTML={this.createMarkup(l.body)} />
-                              </div>
-                            </div>
-
-                            {
-                              user.id
-                            ? <div>
-                                <div className="row">
-                                  <div className="col-md-6">
-                                    <button className="upvote btn-success"  onClick={() => this.props.dispatchUpvote(l)}>Upvote</button>
-                                  </div>
-                                </div>
-
-                                <div className="row">
-                                  <div className="col-md-6">
-                                    <button className="downvote btn-success"  onClick={() => this.props.dispatchDownvote(l)}>Downvote</button>
-                                  </div>
-                                </div>
-                              </div>
-                            : <div>
-                                <h4>
-                                  <Link className="loginSignupButton" to='/login'><strong>Log in </strong></Link>or
-                                  <Link className="loginSignupButton" to='/signup'><strong> sign up </strong></Link> to vote!
-                                </h4>
-                              </div>
-                            }
-
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </li>
-            );
-          })
-        }
-
-                {
-          stack && stack.map(l => {
-            return (
-              <li key={ l.vendor_id } className="stackResults results">
-                <div className="center-block" style={{textAlign: 'left'}}>
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-12">
-                        <div className="panel panel-primary">
-                          <div className="panel-heading">
-                            <h2 className="panel-title" style={{display: 'inline'}}>
-                              <img src="/img/so-icon.png" className="vendorIcon" />
-                              <a href={l.url}>{l.title} <span className="glyphicon glyphicon-log-in"></span></a>
-                            </h2>
-                            <div className="" style={{float:'right', padddinRight: '30px'}}>Views: {l.views}</div>
-                          </div>
-
-                          <div className="panel-body">
-
-                            <div className="row">
-                              <div className="col-md-6">Last Modified: {l.modified}</div>
-
-                              <div className="col-md-6">
-                                 Score: {l.comments}
-                              </div>
-
-                              <div className="col-md-6">
-                                 Tags: {l.tags.join(', ')}
-                              </div>
-                            </div>
-
-                            {
-                              user.id
-                            ? <div>
-                                <div className="row">
-                                  <div className="col-md-6">
-                                    <button className="upvote btn-success"  onClick={() => this.props.dispatchUpvote(l)}>Upvote</button>
-                                  </div>
-                                </div>
-
-                                <div className="row">
-                                  <div className="col-md-6">
-                                    <button className="downvote btn-success"  onClick={() => this.props.dispatchDownvote(l)}>Downvote</button>
-                                  </div>
-                                </div>
-                              </div>
-                            : <div>
-                                <h4>
-                                  <Link className="loginSignupButton" to='/login'><strong>Log in </strong></Link>or
-                                  <Link className="loginSignupButton" to='/signup'><strong> sign up </strong></Link> to vote!
-                                </h4>
-                              </div>
-                            }
-
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </li>
-            );
-          })
-        }
+          {
+            git && git.map(l => {
+              return (
+                <li key={ l.vendor_id } className="gitResults results">
+                  <GitComponent
+                    url={ l.url }
+                    title={ l.title }
+                    status={ l.status }
+                    modified={ l.modified }
+                    comments={ l.comments }
+                    body={ l.body }
+                    user={ user }
+                    createMarkup={ this.createMarkup }
+                  />
+                </li>
+              );
+            })
+          }
+          {
+            stack && stack.map(l => {
+              return (
+                <li key={ l.vendor_id } className="stackResults results">
+                  <StackComponent
+                    url={ l.url }
+                    title={ l.title }
+                    views= { l.views }
+                    modified={ l.modified }
+                    comments={ l.comments }
+                    tags={ l.tags }
+                    user={ user }
+                  />
+                </li>
+              );
+            })
+          }
         </ul>
 
-</div>
+    </div>
 
     );
 
---
+++ client/components/StackComponent.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+module.exports = ({ url, title, views, modified, comments, tags, user }) =>
+  (
+    <div className="center-block" style={{textAlign: 'left'}}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="panel panel-primary">
+              <div className="panel-heading">
+                <h2 className="panel-title" style={{display: 'inline'}}>
+                  <img src="/img/so-icon.png" className="vendorIcon" />
+                  <a href={url}>{title} <span className="glyphicon glyphicon-log-in" /></a>
+                </h2>
+                <div className="" style={{float:'right', padddinRight: '30px'}}>Views: {views}</div>
+              </div>
+
+              <div className="panel-body">
+
+                <div className="row">
+                  <div className="col-md-6">Last Modified: {modified}</div>
+
+                  <div className="col-md-6">
+                      Score: {comments}
+                  </div>
+
+                  <div className="col-md-6">
+                      Tags: {tags.join(', ')}
+                  </div>
+                </div>
+
+                {
+                  user.id
+                ? <div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <button className="upvote btn-success"  onClick={() => this.props.dispatchUpvote(l)}>Upvote</button>
+                      </div>
+                    </div>
+
+                    <div className="row">
+                      <div className="col-md-6">
+                        <button className="downvote btn-success"  onClick={() => this.props.dispatchDownvote(l)}>Downvote</button>
+                      </div>
+                    </div>
+                  </div>
+                : <div>
+                    <h4>
+                      <Link className="loginSignupButton" to='/login'><strong>Log in </strong></Link>or
+                      <Link className="loginSignupButton" to='/signup'><strong> sign up </strong></Link> to vote!
+                    </h4>
+                  </div>
+                }
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
