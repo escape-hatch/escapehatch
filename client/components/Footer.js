9f065055597ac0558ea3f25f20853b6748d4790b
Jen Tam <jennifer.k.tam@gmail.com>
2017-07-06 11:30:36 -0700

    Feat(Favicon): added icon for tab

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -4,12 +4,19 @@ import base64url from 'base64-url';
 
 module.exports = () =>
   (
-  <div id="footer">
-        <div className="container">
-        <p>© 2017 All Rights Reserved. Privacy and Legal.</p>
-          <p className="muted credit">Made for developers <a href="https://github.com/escape-hatch/escapehatch"><i className="fa fa-code" aria-hidden="true"></i>
-            </a>
-          </p>
-        </div>
+  <div>
+    <div id="footer">
+          <div className="container">
+          <p>© 2017 All Rights Reserved. Privacy and Legal.</p>
+            <p className="muted credit">Made for developers <a href="https://github.com/escape-hatch/escapehatch"><i className="fa fa-code" aria-hidden="true"></i>
+              </a>
+            </p>
+          </div>
+    </div>
+
   </div>
   );
+
+    // <ul className="nav pull-right scroll-top">
+    //   <li><a className="scroll" href="#" title="Scroll to top"><i className="glyphicon glyphicon-chevron-up"></i></a></li>
+    // </ul>
